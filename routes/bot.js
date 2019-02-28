ff74e1d8c3428fca18a28d9b6fceb2a773cebb24
khalilbenmiled <khalil.benmiled@esprit.tn>
2019-02-28 21:38:43 +0100

    git

--- routes/bot.js
+++ routes/bot.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/',function(req,res){
-    res.send("Hello Boot")
+    res.send("Hello Bot")
 })
 
 module.exports = router;
